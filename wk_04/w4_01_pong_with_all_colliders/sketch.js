75ce3d69ad92cf140685cb5e9179bac786ed642a
fozzest <31965704+fozzest@users.noreply.github.com>
2018-02-25 16:17:02 -0500

    finally

--- wk_04/w4_01_pong_with_all_colliders/sketch.js
+++ wk_04/w4_01_pong_with_all_colliders/sketch.js
@@ -105,12 +105,12 @@ function drawScene(whichScene) {
       textSize(80);
       textAlign(CENTER, CENTER);
       text("Welcome to Pong", width/2, height/2);
-      text("press the mouse to continue", width/2, height/2);
+      text("press the mouse to continue", width/2, height/2 + 120);
       break;
     case sceneState.TUTORIAL:
       if (millis() > tutorialTimer + timeForTutorial) {
         background(0);
-        fill(0);
+        fill(255);
         textSize(48);
         textAlign(CENTER, CENTER);
         text("HOW TO PLAY...", width/2, height/2 - 100);
@@ -122,7 +122,7 @@ function drawScene(whichScene) {
         
       } else {
         background(0);
-        fill(0);
+        fill(255);
         textSize(48);
         textAlign(CENTER, CENTER);
         text("HOW TO PLAY...", width/2, height/2 - 100);
@@ -213,7 +213,10 @@ function checkTransition(whichScene) {
     //if the ball hits a crtain amount of times, queue another collider
     //if score +1 (aka point lost), some colliders exit
     //idea is that as score increases, as well as paddle hits, colliders increase
-    
+    if (colliding = true){
+      colliders.push(new Bryan());
+
+    }
 
 
       if (p1Score >= 10 || p2Score >= 10) {
