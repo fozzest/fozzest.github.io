ebb8a56cd9f6136760c48a852f11f216329032de
fozzest <31965704+fozzest@users.noreply.github.com>
2018-02-25 18:28:17 -0500

    new

--- wk_04/w4_01_pong_with_all_colliders/sketch.js
+++ wk_04/w4_01_pong_with_all_colliders/sketch.js
@@ -204,204 +204,6 @@ function everything() {
 
 
 
-
-
-function checkCollisionWithBall(ball, other) {
-  if (ball.pos.x + ball.width/2 > other.pos.x && 
-      ball.pos.x + ball.width/2 < other.pos.x + other.width && 
-      ball.pos.y + ball.height/2 > other.pos.y &&
-      ball.pos.y + ball.height/2 < other.pos.y + other.height) {
-    ball.collided(other);
-    other.collided(ball);
-    colliding = true;
-  }
-}
-
-function Ball() {
-  this.pos = createVector(width/2, height/2);
-  this.vel = createVector(0, 0);
-  this.angle = random(TWO_PI);
-  this.speed = 7;
-  this.vel.x = cos(this.angle) * this.speed;
-  this.vel.y = sin(this.angle) * this.speed;
-  this.width = 15;
-  this.height = 15;
-
-  this.update = function() {
-    if (this.pos.x < -this.width) {
-      p2Score++;
-      this.resetAfterPoint(0);
-    } else if (this.pos.x > width) {
-      p1Score++;
-      this.resetAfterPoint(1);
-    }
-
-    if (this.pos.y < margin || 
-        this.pos.y > height - margin - this.height) {
-      this.vel.y *= -1;
-    }
-
-    this.pos.add(this.vel);
-  };
-
-  this.display = function() {
-    noStroke();
-    fill(255);
-    rectMode(CORNER);
-    rect(this.pos.x, this.pos.y, this.width, this.height);
-  }
-
-  this.resetAfterPoint = function(whichPlayer) {
-    this.pos = createVector(width/2, height/2);
-    this.vel = createVector(0, 0);
-    this.speed = 7;
-    if (whichPlayer === 1) {
-      this.getStartingAngle(4 * PI/6, 8 * PI/6);
-    } else if (whichPlayer === 0) {
-      this.getStartingAngle(-PI/3, PI/3);
-    }
-  }
-
-  this.getStartingAngle = function(angleLow, angleHigh) {  
-    var angle = random(angleLow, angleHigh);
-    this.vel.x = cos(angle) * this.speed;
-    this.vel.y = sin(angle) * this.speed;
-  }
-
-  this.collided = function(other) {
-    
-  }
-};
-
-function Paddle(num) {
-  this.num = num;
-  this.width = 15;
-  this.height = 80;
-  if (num == 0) {
-    this.pos = createVector(margin, height/2);
-  } else {
-    this.pos = createVector(width-this.width-margin, height/2);
-  }
-  this.vel = createVector(0, 0);
-
-  this.update = function() {
-    this.pos.add(this.vel);
-  }
-
-  this.display = function() {
-    noStroke();
-    fill(255);
-    rectMode(CORNER);
-    rect(this.pos.x, this.pos.y, this.width, this.height);
-  }
-
-  this.move = function(up, down) {
-    this.vel.y = 0;
-    if (up) {
-      if (this.pos.y > margin) {
-        this.vel.y = -5;
-      } else {
-        this.pos.y = margin;
-      } 
-    }
-    if (down) {
-      if (this.pos.y + this.height < height - margin) {
-        this.vel.y = 5;
-      } else {
-        this.pos.y = height - this.height - margin;
-      }
-    } 
-  }
-
-  this.collided = function(other) {
-    var diff = (other.pos.y + other.height/2) - this.pos.y;
-    if (this.num === 0) {
-      angle = map(diff, 0, this.height, -PI/3, PI/3);
-    }
-    if (this.num === 1) {
-      angle = map(diff, this.height, 0, 4*PI/6, 8*PI/6);
-    }
-    other.speed += 1;
-    other.vel.x = cos(angle) * other.speed;
-    other.vel.y = sin(angle) * other.speed;
-    paddleBounceSFX.play();
-  }
-}
-
-
-
-function keyPressed() {
-  if (key === ' ') {
-    switch (floor(random(9))) {
-      case 0:
-        colliders.push(new Bryan());
-        break;
-      case 1:
-        colliders.push(new Yizhou());
-        break;
-      case 2:
-        colliders.push(new Ellie());
-        break;
-      case 3:
-        colliders.push(new Yanwen());
-        break;
-      case 4:
-        colliders.push(new MaddyRed());
-        colliders.push(new MaddyGreen());
-        colliders.push(new MaddyBlue());
-        break;
-      case 5:
-        colliders.push(new AlyssaForrest());
-        break;
-      case 6:
-        colliders.push(new Sarah());
-        break;
-      case 7:
-        colliders.push(new Jackie());
-        break;
-      case 8:
-        colliders,push(new Cat());
-        break;
-    }
-  }
-
-  if (key === 'W') {
-    p1Up = true;
-  }
-  if (key === 'S') {
-    p1Down = true;
-  }
-
-  if (keyCode === UP_ARROW) {
-    p2Up = true;
-  }
-  if (keyCode === DOWN_ARROW) {
-    p2Down = true;
-  }
-}
-
-function keyReleased() {
-  if (key === 'W') {
-    p1Up = false;
-  }
-  if (key === 'S') {
-    p1Down = false;
-  }
-
-  if (keyCode === UP_ARROW) {
-    p2Up = false;
-  }
-  if (keyCode === DOWN_ARROW) {
-    p2Down = false;
-  }
-}
-
-
-
-//SCENES
-
-
-
 function checkTransition(whichScene) {
   switch (whichScene) {
     case sceneState.INTRO:
@@ -526,63 +328,58 @@ function drawCollide(whichCollide) {
 
 function checkLevelUP(whichCollide) {
   switch (whichCollide) {
-    case sceneState.INTRO:
-      if (mouseOn) {
-        currentState++;
-        setUpCollider(currentState);
-      }
       case collidestate.ZERO:
       if (p1Score>=1 || p2Score >=1){
         currentState++;
-        setUpCollider(currentState);
+        setUpCollider(currentCollider);
       }
       break;
     case collideState.ONE:
       if (p1Score>=2 || p2Score >=2){
         currentState++;
-        setUpCollider(currentState);
+        setUpCollider(currentCollider);
       }
       break;
     case collideState.TWO:
       if (p1Score>=3 || p2Score >=3){
         currentState++;
-        setUpCollider(currentState);
+        setUpCollider(currentCollider);
       }
       break;
     case collideState.THREE:
       if (p1Score>=4 || p2Score >=4){
         currentState++;
-        setUpCollider(currentState);
+        setUpCollider(currentCollider);
       }
       break; 
     case collideState.FOUR:
       if (p1Score>=5 || p2Score >=5){
         currentState++;
-        setUpCollider(currentState);
+        setUpCollider(currentCollider);
       }
         break;
     case collideState.FIVE:
       if (p1Score>=6 || p2Score >=6){
         currentState++;
-        setUpCollider(currentState);
+        setUpCollider(currentCollider);
       }
         break;
     case collideState.SIX:
       if (p1Score>=7 || p2Score >=7){
         currentState++;
-        setUpCollider(currentState);
+        setUpCollider(currentCollider);
       }
         break;
     case collideSTATE.SEVEN:
     if (p1Score>=8 || p2Score >=8){
         currentState++;
-        setUpCollider(currentState);
+        setUpCollider(currentCollider);
       }
         break;
     case collideSTATE.EIGHT:
       if (p1Score>=9 || p2Score >=9){
         currentState++;
-        setUpCollider(currentState);
+        setUpCollider(currentCollider);
       }
     default:
       break;
