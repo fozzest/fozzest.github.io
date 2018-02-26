40ebc8d0b1e25273adeef54077d3e626971f1876
fozzest <31965704+fozzest@users.noreply.github.com>
2018-02-25 21:05:22 -0500

    new

--- wk_04/w4_01_pong_with_all_colliders/sketch.js
+++ wk_04/w4_01_pong_with_all_colliders/sketch.js
@@ -541,25 +541,25 @@ function checkLevelUP(whichCollide) {
   switch (whichCollide) {
     
       case collideState.ZERO:
-      if (p1Score>=1 && ball.pos.x =0 || p2Score >=1 && ball.pos.x =0){
+      if (p1Score>1 && ball.pos.x =0 || p2Score >1 && ball.pos.x =0){
         currentCollider++;
         setUpCollider(currentCollider);
       }
       break;
     case collideState.ONE:
-      if (p1Score>=2 && ball.pos.x =0 || p2Score >=2 && ball.pos.x =0){
+      if (p1Score>2 && ball.pos.x =0 || p2Score >2 && ball.pos.x =0){
         currentCollider++;
         setUpCollider(currentCollider);
       }
       break;
     case collideState.TWO:
-      if (p1Score>=3 && ball.pos.x =0 || p2Score >=3 && ball.pos.x =0){
+      if (p1Score>3 && ball.pos.x =0 || p2Score >3 && ball.pos.x =0){
         currentCollider++;
         setUpCollider(currentCollider);
       }
       break;
     case collideState.THREE:
-      if (p1Score>=4 && ball.pos.x =0 || p2Score >=4 && ball.pos.x =0){
+      if (p1Score>4 && ball.pos.x =0 || p2Score >4 && ball.pos.x =0){
         currentCollider++;
         setUpCollider(currentCollider);
       }
