b8f64c233b066c57e4507e323cb6955fb8e61276
fozzest <31965704+fozzest@users.noreply.github.com>
2018-03-03 17:40:35 -0500

    var

--- wk_05/wk_05_paint/sketch.js
+++ wk_05/wk_05_paint/sketch.js
@@ -35,8 +35,8 @@ function PaintMark(position) {
     for (var k = 0; k <= width; k+=20) {
     for (var j = 0; j <= height; j+=20) {
       
-      float r = map(k, 0, width, 0, 255);
-      float g = map(j, 0, height, 0, 255);
+      var r = map(k, 0, width, 0, 255);
+      var g = map(j, 0, height, 0, 255);
       
       fill(r, g, 125);
     
