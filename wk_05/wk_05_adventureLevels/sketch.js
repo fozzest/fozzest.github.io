e597a1fe6b24c97d47bfe4b18e855412421007aa
fozzest <31965704+fozzest@users.noreply.github.com>
2018-03-05 11:58:56 -0500

    new

--- wk_05/wk_05_adventureLevels/sketch.js
+++ wk_05/wk_05_adventureLevels/sketch.js
@@ -63,7 +63,7 @@ function Scene(sceneText, bg, options, rectPos1, rectPos2, rectPos3, rectPos4, n
       rect(0,0,width,height);
       fill(255);
       text(this.options[i]), 100, 100;
-      text('OPTION ' + (i) + ': ' + this.options[i], 150, 200 + i * 50);
+      text('Number or' + ': ' + this.options[i], 150, 200 + i * 50);
 
       fill (201, 118, 197);
       rect(this.rectPos1, this.rectPos2, 40,40);
@@ -73,7 +73,7 @@ function Scene(sceneText, bg, options, rectPos1, rectPos2, rectPos3, rectPos4, n
 
         if (mouseIsPressed && mouseX > this.rectPos1 && mouseX < this.rectPos1 +40 && mouseY > this.rectPos2 && mouseY < this.rectPos1 +40){
     console.log("click");
-    var scenes = scenes[i+1];
+    this.nextScenes;
   }
       
     }
