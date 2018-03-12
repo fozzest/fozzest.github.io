409915901cdcf398318400ece16be65897cba4d4
fozzest <31965704+fozzest@users.noreply.github.com>
2018-03-11 21:43:53 -0400

    shark

--- Code02/midterm/midterm_sketch/sketch.js
+++ Code02/midterm/midterm_sketch/sketch.js
@@ -20,10 +20,10 @@ var keyOn = false;
 
 
 var diver;
-var ground;
-var shark;
+var bubble;
+var shark = [];
 var bg;
-var gameOver;
+var bubbleImg;
 var diverImg; 
 var sharkImg; 
 var groundImg;
@@ -42,17 +42,14 @@ function setup() {
 
   diverImg = loadImage("assets/diver.png");
   sharkImg = loadImage("assets/shark.png");
-  //groundImg = loadImage("assets/flappy_ground.png");
+  bubbleImg = loadImage("assets/bubble.png");
   bgImg = loadImage("assets/water.png");
   
 
   diver = new Diver();
 
-  shark = new Shark();
+  shark = new Shark(random(0,width),height);
 
-  
-  
-  
 
 }
 
@@ -72,6 +69,7 @@ function draw() {
       text("welcome to the\nDiving Game\n\"press SPACE to continue\"", width/2, height/2);
       break;
     case sceneState.DIRECTIONS:
+        var millisecond = millis();
         background(150, 200, 200);
         fill(0);
         textSize(48);
@@ -80,12 +78,17 @@ function draw() {
         textSize(32);
         text("try to hit a key exactly when\nthe counter hits zero", width/2, height/2)
     case sceneState.GAME:
-        image(bgImg, 0,0, width, height);
+
+
+
+
+
+    image(bgImg, 0,0, width, height);
 
         //image(sharkImg, 100,height-100, 70,70);
         //image(diverImg, width/2 + diverMove, height/2+200, 50, 50);
-        shark.update();
-        shark.display(50);
+        //shark.update();
+        //shark.display(50);
         diver.update();
         diver.display();
         fill (255,0,0);
@@ -98,6 +101,23 @@ function draw() {
         if (millisecond % 1000){
             airCounter= airCounter -.07;
           }
+
+          // if (millisecond % 10000){
+          //   shark.push(new Shark(random(0,width), height));
+          // }
+
+        for (var i = 0; i < shark.length; i++) {
+          shark[i].update();
+         }
+       for (var i = 0; i < shark.length; i++) {
+          shark[i].display();
+           }
+
+
+
+
+
+
       break;
       default:
       break;
@@ -105,6 +125,17 @@ function draw() {
     }
 
   }
+
+  function newShark() {
+  var millisecond = millis();
+  if (millisecond % 1000){
+        shark.push(new Shark(random(0,width), height/2));
+        console.log("new");
+      }
+    }
+
+
+  
 
 
   function checkTransition(whichScene) {
@@ -155,12 +186,6 @@ function keyPressed() {
   keyOn = true;
 }
 
-  
-  
-  
-
-
-
 
 
 function Diver() {
@@ -180,134 +205,52 @@ function Diver() {
   }
 }
 
+ // function Shark(sharkX, sharkY) {
 
+ //    this.dx;
+ //    this.dy = -3;
 
-
-
- function Shark() {
+ //   this.width = 50;
+ //   this.height = 50;
+ //   this.pos = createVector(width/2, height/2 +200);
   
-   this.width = 50;
-   this.height = 50;
-   this.pos = createVector(width/2, height/2 +200);
-  
 
-   this.update = function() {
+ //   this.update = function() {
+ //     this.sharkY += this.dy;
+ //   }
+
+ //   this.display = function() {
+    
+ //    //image(sharkImg, 100,height-100, 70,70);
+ //    image(sharkImg, sharkX, sharkY, this.width, this.height);
+
+    
+    
+ //    }
+ //   }
+ 
+
+  function Shark(sharkX, sharkY) {
+     this.speed = 1;
+     this.angle = 0;
+     this.vel = 1;
+
+     this.width = 50;
+     this.height = 50;
+     this.pos = createVector(this.sharkX, this.sharkY);
+     this.update = function() {
+  
+     this.vel.y =  this.speed;
      this.pos.add(this.vel);
    }
 
-   this.display = function(sharkX) {
-    var sharkY = 0;
-    //image(sharkImg, 100,height-100, 70,70);
-    image(sharkImg, sharkX, this.pos.y + sharkY , this.width, this.height);
-
-    
-    if (this.pos.y < height){
-      sharkY = 5;
-    }
-    }
+     this.display = function() {
+     fimage(sharkImg, this.sharkX, this.sharkY, this.width, this.height);
    }
- 
+ }
 
 
 
   
   
 
-
-
-// function keyPressed() {
-//   if (keyCode === LEFT_ARROW) {
-//     diverMoveLEFT = true;
-//   } else if (keyCode === RIGHT_ARROW) {
-//     diverMoveRight = true;
-//   }
-// }
-
-
-// function keyReleased() {
-//   if (keyCode === LEFT_ARROW) {
-//     diverMoveLEFT = false;
-//   } else if (keyCode === RIGHT_ARROW) {
-//     diverMoveRight = false;
-//   }
-// }
-
-//   if(gameOver && keyWentDown("x"))
-//     newGame();
-
-//   if(!gameOver) {
-
-//     if(keyWentDown("x"))
-//       bird.velocity.y = FLAP;
-    
-//     bird.velocity.y += GRAVITY;
-    
-//     if(bird.position.y<0)
-//       bird.position.y = 0;
-    
-//     if(bird.position.y+bird.height/2 > GROUND_Y)
-//       die();
-
-//     if(bird.overlap(pipes))
-//       die();
-
-//     //spawn pipes
-//     if(frameCount%60 == 0) {
-//       var pipeH = random(50, 300);
-//       var pipe = createSprite(bird.position.x + width, GROUND_Y-pipeH/2+1+100, 80, pipeH);
-//       pipe.addImage(pipeImg);
-//       pipes.add(pipe);
-
-//       //top pipe
-//       if(pipeH<200) {
-//         pipeH = height - (height-GROUND_Y)-(pipeH+MIN_OPENING);
-//         pipe = createSprite(bird.position.x + width, pipeH/2-100, 80, pipeH);
-//         pipe.mirrorY(-1);
-//         pipe.addImage(pipeImg);
-//         pipes.add(pipe);
-//       }
-//     }
-
-//     //get rid of passed pipes
-//     for(var i = 0; i<pipes.length; i++)
-//       if(pipes[i].position.x < bird.position.x-width/2)
-//         pipes[i].remove();
-//   }
-
-//   camera.position.x = bird.position.x + width/4;
-
-//   //wrap ground
-//   if(camera.position.x > ground.position.x-ground.width+width/2)
-//     ground.position.x+=ground.width;
-
-//   background(247, 134, 131); 
-//   camera.off();
-//   image(bgImg, 0, GROUND_Y-190);
-//   camera.on();
-
-//   drawSprites(pipes);
-//   drawSprite(ground);
-//   drawSprite(bird);
-// }
-
-// function die() {
-//   updateSprites(false);
-//   gameOver = true;   
-// }
-
-// function newGame() {
-//   pipes.removeSprites();
-//   gameOver = false;
-//   updateSprites(true);
-//   bird.position.x = width/2;
-//   bird.position.y = height/2;
-//   bird.velocity.y = 0;
-//   ground.position.x = 800/2;
-//   ground.position.y = GROUND_Y+100;
-// }
-
-// function mousePressed() {
-//   if(gameOver)
-//     newGame();
-//   bird.velocity.y = FLAP;
-// }
