ec2d40b49f40a3f7f212156e053ce1cfede36eb1
fozzest <31965704+fozzest@users.noreply.github.com>
2018-03-26 00:28:58 -0400

    midterm

--- Code02/midterm/midterm_sketch/sketch.js
+++ Code02/midterm/midterm_sketch/sketch.js
@@ -1,141 +1,186 @@
+//Forrest Whitcomb
+
 // code 2
 // bfa dt
 // spring 2018
-// bryan ma
+// w/ bryan ma
 
 // midterm
 
-
 var sceneState = {
   INTRO: 0,
-  TUTORIAL: 1,
-  GAME: 2,
-  WIN: 3,
-  LOSE: 4
-};
+  LEVEL1: 1,
+  LEVEL2: 2,
+  LEVEL3: 3,
+  LEVEL4: 4,
+  END: 5
+}
 
 var currentState = sceneState.INTRO;
-
+var sceneData;
+var scenesData = [];
+var switching = 0;
 var keyOn = false;
 
 
-var diver;
-var bubble;
-var shark = [];
+var sharks;
 var bg;
-var bubbleImg;
-var diverImg; 
-var sharkImg; 
-var groundImg;
 var bgImg;
-var easing = 0.05;
-var diverMove;
-
-var diverMoveLEFT = false;
-var diverMoveRIGHT = false;
-
-var airCounter = 0;
+var diver;
+var diverImg;
+var gold; 
+var goldImg;
 
 
-
-
-var sceneData;
-
-var currentScene = 0;
-var scenes = [];
-
-
-// function preload() {
-//   sceneData = loadJSON('scenes.json');
-// }
-
+function preload(){
+  sceneData = loadJSON('scenes.json');
+  diverImg = loadImage("assets/diver.png");
+  goldImg = loadImage("assets/gold.png");
+  bgImg = loadImage("assets/bg.png");
+}
 
 function setup() {
-  createCanvas(400,600);
-  //CreateScenesFromData(sceneData.scenes);
-
-  diverImg = loadImage("assets/diver.png");
-  sharkImg = loadImage("assets/shark.png");
-  bubbleImg = loadImage("assets/bubble.png");
-  bgImg = loadImage("assets/water.png");
-  
-
+createCanvas(800,800);
+  //see flappyBird line 25, 
+  //http://p5play.molleindustria.org/examples/index.html?fileName=flappyBird.js
+  //continuing to gain understanding post-pong project
   diver = new Diver();
-
-  shark = new Shark(random(0,width),height);
-
+  gold = new Gold();
+  sharks = new Group();
 
 }
 
 function draw() {
   drawScene(currentState);
   checkTransition(currentState);
-  keyOn = false;
+  keyOn = false; 
 }
 
 
-
-//SCENES
-
-
-  function drawScene(whichScene) {
-  switch (currentState) {
+function drawScene(scene) {
+  switch (currentState){
     case sceneState.INTRO:
-      background(20,20,20);
-      fill(255);
+        background(0);
+        fill(255, 0, 0);
+        textSize(40);
+        textAlign(CENTER, CENTER);
+        text("Welcome", width/2, height/2-150);
+        text("to my hardly working midtern", width/2, height/2-100);
+        text("Press Enter to begin", width/2, height/2-50);
+        text("Collect the Gold to advance", width/2, height/2);
+   
+    break;
+
+    case sceneState.LEVEL1:
+      image(bgImg, 0,-100, width, height + 100);
+      fill(255, 0, 0);
       textSize(20);
       textAlign(CENTER, CENTER);
-      text("welcome to the\nDiving Game\n\"press SPACE to continue\"", width/2, height/2);
-      break;
-    case sceneState.DIRECTIONS:
-       
-        background(150, 200, 200);
-        fill(255);
-        
-    case sceneState.GAME:
-        image(bgImg, 0,0, width, height);
+      text(sceneData.scenesData[0].scene, width/2, height - 80);
+      //http://p5play.molleindustria.org/examples/index.html?fileName=flappyBird.js
+      //flappy bird example helped to show how to spawn objects via for loop 
+      if (switching == 0){
+        for ( var i = 0; i < sceneData.scenesData[0].sharkNum[0]; i ++){          
+          var pos1 = createVector(sceneData.scenesData[0].sharkPos[i].x, 
+          sceneData.scenesData[0].sharkPos[i].y);
+          createShark(pos1.x , pos1.y, sceneData.scenesData[0].sharkEase[0]);
+          switching = 1;
+        }
+      }
+      //http://p5play.molleindustria.org/examples/index.html?fileName=sprite4.js
+      //struggled to get asset to travel to mouse, currently using an attractionPoint temporarily
+      gold.display(sceneData.scenesData[0].goldX[0], sceneData.scenesData[0].goldY[0]);
+      diver.display();
+      diver.attractionPoint(1, mouseX, mouseY); 
+      drawSprites(); 
+   
+    break;
 
-        //image(sharkImg, 100,height-100, 70,70);
-        //image(diverImg, width/2 + diverMove, height/2+200, 50, 50);
-        //shark.update();
-        //shark.display(50);
-        diver.update();
-        diver.display();
-        fill (255,0,0);
-        noStroke();
-        rect(50, height-50, width-100 + airCounter, 10);
+    case sceneState.LEVEL2:
+      image(bgImg, 0,-100, width, height + 100);
+      fill(255, 0, 0);
+      textSize(30);
+      textAlign(CENTER, CENTER);
+      text(sceneData.scenesData[1].scene, width/2, height - 80);
+      
+      if (switching == 1){ 
+        for ( var i = 0; i < sceneData.scenesData[1].sharkNum[0]; i ++){
+          var pos1 = createVector(sceneData.scenesData[1].sharkPos[i].x, 
+          sceneData.scenesData[1].sharkPos[i].y);
+          createShark(pos1.x , pos1.y , sceneData.scenesData[1].sharkEase[0]);
+          switching = 0;
+        }
+      }
+      gold.display(sceneData.scenesData[1].goldX[0], sceneData.scenesData[1].goldY[0]);
+      diver.display();
+      diver.attractionPoint(1, mouseX, mouseY);
+      drawSprites(); 
+    
+    break;
 
-        var millisecond = millis();
-      //text('Milliseconds \nrunning: \n' + millisecond, 5, 40);
+    case sceneState.LEVEL3:
+      image(bgImg, 0,-100, width, height + 100);
+      fill(255, 0, 0);
+      textSize(40);
+      textAlign(CENTER, CENTER);
+      text(sceneData.scenesData[2].scene, width/2, height - 80);
+      
+      if (switching == 0){
+        for ( var i = 0; i < sceneData.scenesData[2].sharkNum[0]; i ++){
+          var pos1 = createVector(sceneData.scenesData[2].sharkPos[i].x, 
+          sceneData.scenesData[2].sharkPos[i].y);
+          createShark(pos1.x , pos1.y , sceneData.scenesData[2].sharkEase[0]);
+          switching = 1;
+        }
+      }
+      gold.display(sceneData.scenesData[2].goldX[0], sceneData.scenesData[2].goldY[0]);
+      diver.display();
+      diver.attractionPoint(1, mouseX, mouseY);
+      drawSprites(); 
+    
+    break;
 
-        if (millisecond % 1000){
-            airCounter= airCounter -.07;
-          }
+    case sceneState.LEVEL4:
+      image(bgImg, 0,-100, width, height + 100);
+      fill(255, 0, 0);
+      textSize(50);
+      textAlign(CENTER, CENTER);
+      text(sceneData.scenesData[3].scene, width/2, height - 80);
 
-          // if (millisecond % 10000){
-          //   shark.push(new Shark(random(0,width), height));
-          // }
+      if (switching == 1){
+        for ( var i = 0; i < sceneData.scenesData[3].sharkNum[0]; i ++){
+          var pos1 = createVector(sceneData.scenesData[3].sharkPos[i].x, 
+          sceneData.scenesData[3].sharkPos[i].y);
+          createShark(pos1.x , pos1.y , sceneData.scenesData[3].sharkEase[0]);
+          switching = 0;
+        }
+      }
+      gold.display(sceneData.scenesData[3].goldX[0], sceneData.scenesData[3].goldY[0]);
+      diver.display();
+      diver.attractionPoint(1, mouseX, mouseY);
+      drawSprites(); 
+    
+    break;
 
-        for (var i = 0; i < shark.length; i++) {
-          shark[i].update();
-         }
-       for (var i = 0; i < shark.length; i++) {
-          shark[i].display();
-           }
+    case sceneState.END:
+      background(0);
+      fill(255, 0,0);
+      textSize(50);
+      textAlign(CENTER, CENTER);
+      text("Let's Go Again", width/2, height/2-100);
+      textSize(40);
+      text("Press Enter to go again", width/2, height/2+100);
 
-      break;
-      default:
-      break;
+    break;
 
-    }
+    default:
 
+    break;
   }
+}
 
 
-//TRANSITIONS
-
-
-  function checkTransition(whichScene) {
+function checkTransition(whichScene) {
   switch (whichScene) {
     case sceneState.INTRO:
       if (keyOn) {
@@ -143,156 +188,180 @@ function draw() {
         setUpScene(currentState);
       }
       break;
-    case sceneState.TUTORIAL:
-      if (keyOn) {
+    case sceneState.LEVEL1:     
         if (keyOn) {
           currentState++;
           setUpScene(currentState);      
         }
-      }
+        if (diver.bounce(gold)) {
+          currentState++;
+          setUpScene(currentState);      
+        }
+        //http://p5play.molleindustria.org/docs/classes/Sprite.html#method-bounce
+        if (diver.bounce(sharks)) {
+          currentState = 5;
+          setUpScene(currentState);      
+        }
       break;
-    case sceneState.GAME:
-      if (airCounter == -50) {
-         {
-          currentState = sceneState.INTRO;      
-        
-        setUpScene(currentState);
-      }
-    }
+    case sceneState.LEVEL2:
+      if (keyOn) {
+          currentState++;
+          setUpScene(currentState);      
+        }
+         if (diver.bounce(gold)) {
+          currentState++;
+          setUpScene(currentState);      
+        }
+        if (diver.bounce(sharks)) {
+          currentState = 5;
+          setUpScene(currentState);      
+        }
+      break;
+    case sceneState.LEVEL3:
+      if (keyOn) {
+          currentState++;
+          setUpScene(currentState);      
+        }
+         if (diver.bounce(gold)) {
+          currentState++;
+          setUpScene(currentState);      
+        }
+        if (diver.bounce(sharks)) {
+          currentState = 5;
+          setUpScene(currentState);      
+        }
+      break;
+    case sceneState.LEVEL4:
+      if (keyOn) {
+          currentState++;
+          setUpScene(currentState);      
+        }
+         if (diver.bounce(gold)) {
+          currentState++;
+          setUpScene(currentState);      
+        }
+        if (diver.bounce(sharks)) {
+          currentState = 5;
+          setUpScene(currentState);      
+        }
+      break;
+    case sceneState.END:
+      if (keyOn) {
+       currentState = 0;
+          setUpScene(currentState);      
+        }
       break;
     default:
       break;
   }
 }
 
-
-function setUpScene(whichScene) {
-  switch (whichScene) {
+function setUpScene(scene){
+  switch (scene) {
     case sceneState.INTRO:
       break;
-    case sceneState.TUTORIAL:
+    case sceneState.LEVEL1:
       break;
-    case sceneState.GAME:
+    case sceneState.LEVEL2:
+      break;
+    case sceneState.LEVEL3:
+      break;
+    case sceneState.LEVEL4:
+      break;
+    case sceneState.END:
       break;
     default:
-      break;
+    break;
   }
 }
 
-
-//ACCESS DATA FOR SCENES - UNDER CONSTRUCTION
-
-      //"sharksLength": 10, //the length of Sharks(enemies) in a level
-      //"sharkVel" : 2,   //speed of sharks along Y
-      //"sharkEase" : 0.1,  //strengh of easing towardss character in sharks
-      //"airCounter": 0.2,  //the speed at which the player loses air
-      //"bubbleLength": 7, //the amount of bubbles in a level for the character to absorb
-      //"harpoon":[0,1],   
-      //"nextScenes": [1]
+function keyPressed(){
+  if (keyCode === ENTER) {
+    keyOn = true;
+  }
+}
+  
 
 
-// function CreateScenesFromData(data) {
-//   for (var i = 0; i < data.length; i++) {
-//     scenes.push(new Scene(data[i].sharksLength, data[i].sharkVel, data[i].sharkEase, data[i].airCounter, data[i].bubbleLength, data[i].harpoon, data[i].nextScenes))
+function createShark( sharkX, sharkY, sEase) {
+
+  var newShark = createSprite(sharkX, sharkY);
+  var sharkImg  = loadImage("assets/shark.png");
+
+  newShark.addImage(sharkImg);
+  //currently broken, need to create update and display functions os this could update iself
+  newShark.attractionPoint(sEase, mouseX, mouseY);
+  newShark.scale = 0.5;
+  newShark.setCollider('circle', 0, 0, 50);
+  sharks.add(newShark);
+  return newShark;
+}
+
+//took forever to find a way to move the asset with mouse...
+function Diver(){
+    this.display = function(){
+      diver.posX = width/2;
+      diver.posY = height/2;
+      diver = createSprite(diver.posY, diver.posX);
+      diver.maxSpeed = 10;
+      diver.setCollider("circle", 0, 0, 50);
+      diver.addImage(diverImg);
+      diver.scale = 0.5; 
+    }
+  }
+
+  function Gold(){
+    this.display = function(goldX, goldY){
+      gold.posX = goldX;
+      gold.posY = goldY;
+      gold = createSprite(gold.posY, gold.posX);
+      gold.setCollider("circle", 0, 0, 60);
+      gold.addImage(goldImg);
+      gold.scale = 0.5; 
+    }
+  }
+
+
+
+
+// function Diver() {
+//   diver = createSprite(width/2, height/2);
+//   diver.pos = createVector(width/2, height/2);
+//   diver.vel = createVector(0, 0);
+//   diver.angle = random(TWO_PI);
+//   diver.speed = 7;
+//   diver.vel.x = 1;
+//   diver.vel.y = 1;
+//   diver.width = 15;
+//   diver.height = 15;
+//   diver.setCollider("circle", 0, 0, 30);
+
+//   this.update = function() {
+
+//     diver.pos.add(this.vel);
+//   };
+
+//   this.display = function() {
+
+//      diver = createSprite(width/2, height/2);
+
+//      diver.addImage(diverImg, diver.pos + mouseX, diver.pos + mouseY, diver.width, diver.height);
+//      diver.scale = 0.5;
 //   }
 // }
 
-// function Scene(sharkslength, sharkVel, sharkEase, airCounter, bubbleLength, harpoon, nextScenes) {
-//   this.sharksLength = sharksLength;
-//   this.sharkVel = sharkVel;
-//   this.sharkEase = sharkEase;
-//   this.airCounter = airCounter;
-//   this.bubbleLength = bubbleLength;
-//   this.harpoon = harpoon;
-//   this.nextScenes = nextScenes;
+// function Diver() {
+   
+//   this.width = 50;
+//   this.height = 50;
+//   this.pos = createVector(width/2,  200);
 
-//}
+//   this.update = function() {
+//     this.pos.add(this.vel);
+//   }
 
+//   this.display = function() {
+//     image(diverImg, this.pos.x + mouseX/2, this.pos.y , this.width, this.height);
+//   }
 
-
-
-
-
-function keyPressed() {
-  keyOn = true;
-}
-
-
-
-//COMPONENTS
-
-function Diver() {
-  
-  this.width = 50;
-  this.height = 50;
-  this.pos = createVector(width/2,  200);
-  
-
-  this.update = function() {
-    this.pos.add(this.vel);
-  }
-
-  this.display = function() {
-
-    image(diverImg, this.pos.x + mouseX/2, this.pos.y , this.width, this.height);
-  }
-}
-
- // function Shark(sharkX, sharkY) {
-
- //    this.dx;
- //    this.dy = -3;
-
- //   this.width = 50;
- //   this.height = 50;
- //   this.pos = createVector(width/2, height/2 +200);
-  
-
- //   this.update = function() {
- //     this.sharkY += this.dy;
- //   }
-
- //   this.display = function() {
-    
- //    //image(sharkImg, 100,height-100, 70,70);
- //    image(sharkImg, sharkX, sharkY, this.width, this.height);
-
-    
-    
- //    }
- //   }
- 
-
-  function Shark(sharkX, sharkY) {
-     this.speed = 1;
-     this.angle = 0;
-     this.vel = 1;
-
-     this.width = 50;
-     this.height = 50;
-     this.pos = createVector(this.sharkX, this.sharkY);
-     this.update = function() {
-  
-     this.vel.y =  this.speed;
-     this.pos.add(this.vel);
-   }
-
-     this.display = function() {
-     fimage(sharkImg, this.sharkX, this.sharkY, this.width, this.height);
-   }
- }
-
-
-  function newShark() {
-  var millisecond = millis();
-  if (millisecond % 1000){
-        shark.push(new Shark(random(0,width), height/2));
-        console.log("new");
-      }
-    }
-
-
-
-  
-  
-
+// }
