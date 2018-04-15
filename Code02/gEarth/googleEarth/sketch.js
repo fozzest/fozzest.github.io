e12c5cc2c5b20f657df5a0a801cf87feaf447d91
fozzest <31965704+fozzest@users.noreply.github.com>
2018-04-15 18:06:21 -0400

    new

--- Code02/gEarth/googleEarth/sketch.js
+++ Code02/gEarth/googleEarth/sketch.js
@@ -2,31 +2,53 @@
 
 var map;
 
+
 function initMap() {
 var myLatlng = {lat: -34.397, lng: 150.644};
   map = new google.maps.Map(document.getElementById('map'), {
     center: myLatlng,
-    zoom: 8
+    zoom: 8,
   });
 
 //used geoJSON.io to quickly make geoJSON that could be imported to the map in one line of code
 //http://geojson.io/#map=16/-37.8287/-214.9433
-map.data.loadGeoJson('markers.json');
+
 
   map.addListener('click', function(e) {
     newMarker(e.latLng, map);
   });
 
+
+  map.data.loadGeoJson('markers.json');
+
 }
+
+
 
 
 //Animation, Marker via https://developers.google.com/maps/documentation/javascript/markers
 
 function newMarker(latLng, map) {
+var houseImg  = "assets/house.png"; 		
   var marker = new google.maps.Marker({
     position: latLng,
     animation: google.maps.Animation.DROP,
-    map: map
+    map: map,
+    icon: houseImg
+  });
+
+  //https://developers.google.com/maps/documentation/javascript/examples/infowindow-simple
+  //"infoWindown google map"
+
+   var journal =
+   	   '<input id="textarea" type="text" name="fname">Write about this place<br>';
+
+    var infowindow = new google.maps.InfoWindow({
+          content: journal
+    });
+
+  marker.addListener('click', function() {
+  infowindow.open(map, marker);
   });
 }
 
@@ -37,3 +59,8 @@ function bounce() {
     marker.setAnimation(google.maps.Animation.BOUNCE);
   }
 }
+
+
+
+
+
