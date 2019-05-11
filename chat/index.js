348c92a383beba1b47aa0470c811922ece8743e0
Forrest Whitcomb <whitf151@newschool.edu>
2019-05-11 10:16:18 +0200

    labFinal02

--- chat/index.js
+++ chat/index.js
@@ -1,6 +1,29 @@
 // Setup basic express server
 // Setup basic express server
 
+
+
+// var app = require('express')();
+// var server = require('http').Server(app);
+// var io = require('socket.io')(server);
+
+// server.listen(80);
+// // WARNING: app.listen(80) will NOT work here!
+
+// app.get('/', function (req, res) {
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+// io.on('connection', function (socket) {
+//   socket.emit('news', { hello: 'world' });
+//   socket.on('drawing', function (data) {
+//     console.log(data);
+//   });
+// socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+  
+// });
+
+
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
